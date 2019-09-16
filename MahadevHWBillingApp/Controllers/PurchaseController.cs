247e3cdaf4fbd92bc925497c8f678d6dbb9d7ea2
rambharlia007 <rambharlia007@gmail.com>
2019-09-17 00:10:51 +0530

    added empty controller and fixed select2 re render issue

---
+++ MahadevHWBillingApp/Controllers/PurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MahadevHWBillingApp.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        // GET: Purchase
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
