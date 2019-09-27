ebb8285bd71c2543766aa3c606b77b409d133ab6
rambharlia <rambharlia007@gmail.com>
2019-09-27 20:13:58 +0530

    report functionality

---
+++ MahadevHWBillingApp/Controllers/ReportController.cs
@@ -0,0 +1,53 @@
+﻿
+using EPPlusEnumerable;
+using MahadevHWBillingApp.Models;
+using OfficeOpenXml;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MahadevHWBillingApp.Controllers
+{
+
+    public class ReportController : BaseController
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Product()
+        {
+            var alp = new[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+            var p = _mahadevHwContext.Purchase.ToList();
+            var excelPackage = Spreadsheet.CreatePackage(p);
+            var cs = excelPackage.Workbook.Worksheets.FirstOrDefault();
+
+            var prop = typeof(Purchase).GetProperties();
+            var count = p.Count + 2;
+
+            var index = 0;
+            foreach (var d in prop)
+            {
+                var let = alp[index++];
+                if (d.PropertyType == typeof(decimal) && !d.Name.Equals("Id"))
+                {
+                    cs.Cells[$"{let}2:{let}{count}"].Style.Numberformat.Format = "#,##0.00";
+                    cs.Cells[$"{let}{count}"].Formula = $"=SUM({let}2:{let}{count-1})";
+                }
+            }
+
+            cs.Name = "Rename";
+
+            cs.Cells[cs.Dimension.Address].AutoFitColumns();
+
+            cs.Cells[cs.Dimension.Address].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Left;
+
+            cs.PrinterSettings.RepeatRows = new ExcelAddress("1:1");
+
+            return File(excelPackage.GetAsByteArray(), "application/vnd.ms-excel", "MySpreadsheet.xlsx");
+        }
+    }
+}
--- MahadevHWBillingApp/Models/Purchase.cs
+++ MahadevHWBillingApp/Models/Purchase.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using EPPlusEnumerable;
 using Microsoft.Extensions.Logging.Internal;
 
 namespace MahadevHWBillingApp.Models
 {
     public class Purchase
     {
+        [SpreadsheetExclude]
         public int Id { get; set; }
         public string BusinessName { get; set; }
         public decimal TotalCGSTAmount { get; set; }
         public decimal TotalSGSTAmount { get; set; }
         public decimal TotalAmount { get; set; }
         public string Invoice { get; set; }
+        [SpreadsheetExclude]
         public DateTime Date { get; set; }
 
         public string FormatDate => Date.ToString("dd-MM-yyyy");
