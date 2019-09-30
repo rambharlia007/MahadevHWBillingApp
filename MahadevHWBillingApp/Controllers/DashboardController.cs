934012eb1e6c5aa27d631dbe53aa248f17940e3a
rambharlia <rambharlia007@gmail.com>
2019-09-30 20:33:58 +0530

    dashboard

---
+++ MahadevHWBillingApp/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MahadevHWBillingApp.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        // GET: Dashboard
+        public ActionResult New()
+        {
+            return View(_profile);
+        }
+    }
+}
